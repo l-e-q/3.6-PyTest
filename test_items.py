6ccef44e0d134cb4261c9321189d482198395fab
o <nesubludai@gmail.com>
2023-02-14 17:10:34 +0300

    Some comments in test_items.py

--- test_items.py
+++ test_items.py
@@ -9,6 +9,8 @@ link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 def test_guest_should_see_add_to_basket_button(browser):
     browser.get(link)
-    browser.implicitly_wait(30)
+    browser.implicitly_wait(2)
+    # uncomment if you need to see page
+    # time.sleep(30)
     add_to_basket_button = EC.visibility_of_element_located((By.CLASS_NAME, 'btn btn-lg btn-primary btn-add-to-basket'))
     assert add_to_basket_button, 'Button not found'
