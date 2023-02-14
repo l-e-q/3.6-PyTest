4813398e29197fbfeae17f8cbeff123400ceee30
o <nesubludai@gmail.com>
2023-02-14 17:08:32 +0300

    test_items.py works correctly!

--- test_items.py
+++ test_items.py
@@ -1,14 +1,14 @@
+import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 from selenium.webdriver.common.by import By
 
-link = 'http://selenium1py.pythonanywhere.com/'
+link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 
-def test_quest_should_see_login_link_pass(browser):
+def test_guest_should_see_add_to_basket_button(browser):
     browser.get(link)
-    browser.find_element(By.CSS_SELECTOR, '#login_link')
-
-
-def test_quest_should_see_login_link_fail(browser):
-    browser.get(link)
-    browser.find_element(By.CSS_SELECTOR, '#magic_link')
-
+    browser.implicitly_wait(30)
+    add_to_basket_button = EC.visibility_of_element_located((By.CLASS_NAME, 'btn btn-lg btn-primary btn-add-to-basket'))
+    assert add_to_basket_button, 'Button not found'
